4ba9608392de114632efde92818ade8acdbec637
gasparRuben01 <gaspar.zanellini@gmail.com>
2017-10-10 03:57:08 -0300

    ll

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,123 +1,131 @@
-//una preposition tiene tanto rules como facts
-class Preposition{
+class Relationship {
 	constructor(name){
 		this.name=name;
-		this.facts={};
-		this.rules=[];
-		this.separator='_';
-		this.formFact=function(args){
-			return Array.prototype.slice.call(args, 0).join(separator);}
+		this.facts=new Set();
+		this.rules=null;
 	}
-
-	addFact(args){
-		this.facts[formFact(args)]=true;
+	addFact(fact){
+		fact=fact.join(',');
+		this.facts.add(fact);
 	}
-
-	hasFact(args){
-		return this.facts[formFact(args)];
+	setRules(rules){
+		this.rules=rules;
 	}
-	addRule(rule, orderofvariables){
-		this.rules.push([rule, orderofvariables])
-	}
-	evaluate(args){
-		if(hasFacts(formFact(args))){
+	areRelated(args, db){
+		var fact=args.join(',');
+		if(this.facts.has(fact))
 			return true;
+		return this.rules ? this.rules.map(function(r){r.areRelated(args, db)}).indexOf(false) : false;
+	}
+
+}
+
+class Rule {
+	constructor(relation, orderOfargs){
+		this.relation=relation;
+		this.orderOfargs=orderOfargs;
+	}	
+	areRelated(args, db){
+		if(args.length>=this.orderOfargs.length){
+			return false;
 		}
-		var retorno=true;
-		this.rules.forEach(function(rule_order, indice, array){
-			var ruleArgs=[];
-			rule_order[1].forEach(function(pos, indice, array){
-				ruleArgs.push(args[pos])});
-				
-			if(rule_order[0].evaluate(ruleArgs)){
-				retorno=false;
-			}
-		})
-		return retorno;
+		return db[relation] ? db[relation].areRelated(this.orderOfargs.map(function(pos){return args[pos]}), db) : false;
 	}
 }
 
-		
-var Interpreter = function () {
-	var prepositions={};
-	var preposition=null;
-	var nombre=/^\s*([A-Za-z]*)/;
-	var argumentos=/^\s*\)/;
-	var argumento=/^\s*([A-Za-z]*)\s*([,)])/;
-	var rule=/^s*:=/;
-	var getPrepositonFromBuffer=function(buffer){
-					var preposition=prepositions[buffer.name];
-					if(!preposition){
-						preposition=new Preposition(buffer.name);
-						prepositions[preposition.name]=preposition;
-					}
-					preposition.addFact(buffer.args);
-					buffer.rules.forEach(function(r, ind, array){
-						var rule_name=r[0];
-						var rule_order_variales=r[1];
-						var rule=prepositons[rule_name]
-						if(rule)
-							throw Error();
-						preposition.addRule(rule, rule_order_variables);
-					})
-					
-				     };
+var Parser=function(){
+	var RULESYMBOL=/:=/;
+	var NAME_ARGS=/\s*[A-Za-z]+\s*\(.*\)/;
+	var END=/\.$/;
+	var NAME=/^\s*([A-Za-z]+)\s*/;
 
-
-	var parse_name_arguments=function(input){
-		var buffer=null;
-		if(!input.match(nombre))
-			throw Error();
-		else{
-			buffer["name"]=input.match(nombre)[1];	
-			input=input.replace(nombre)="";
-		}
-		if(!input.match(argumentos))
-			throw Error();
-		else
-			input=input.replace(argumentos)="";
-
-		var args=[];
-		while(input.match(argumento)){
-			args.push(input.match(argumento)[1]);
-			var last=input.match(arguemnto)[2];
-			input=input.replace(argumento, "");
-			if(last==")")
-				break;
-		}
-		buffer["args"]=args;
-		return [buffer, input];
+	var getName=function(name_args){
+		return name_args.match(NAME_ARGS) && name_args.match(NAME) ? name_args.match(NAME)[1] : null;
 	};
+	var getVars=function(name_args){
+		if(!name_args.match(NAME_ARGS)){
+			return null;
+		}
+		var args=name_args.replace(NAME, "");
+		args=args.replace(/\(/, "");
+		args=args.replace(/\)/,"");
+		args=args.split(/,/).map(function(a){ return a.match(NAME) ? a.match(NAME)[1] : null});
+		return args.indexOf(null)==-1 ? args : null;
+	};
+		
+
+
+	this.parseRule=function(sentence){
+			if(!sentence.match(END))
+				return null;
+				
+			sentence=sentence.replace(END, "");
+			sentence=sentence.split(RULESYMBOL);
+			if(sentence.length!=2)
+				return null;
+			sentence[1]=sentence[1].split(/,/);
+			sentence=sentence[0].concat(sentence[1]);
+			name_args=sentence.map(function(a){return getName(a) && getVars(a) ? {name:getName(a), args:getVars(a)} : null});
+			if(args.indexOf(null))
+				return null;
+			var name=name_args[0]["name"];
+			var args=name_args[0]["args"];
+			name_args=name_args.slince(1);
+			var rules=name_args.map(function(x){ 
+						var orderofargs=x.args.map(function(a){ return args.indexOf(a)!=-1 ? args.indexOf(a) : null});
+						return orderofargs.indexOf(null)!=-1 ?  null : new Rule(x.name, orderofargs);});
+			return rules.indexOf(null)!=-1 ? null : {name:name, rules:rules}};
+
+	this.parseFact=function(sentence){
+		if(!sentence.match(END))
+			return null;
+		sentence=sentence.replace(END, "");
+		return getName(sentence) && getVars(sentence) ? {name:getName(sentence), fact:getVars(sentence)} : null;};
+}
+					
+
+
+				
+
+
+			
+
+
+
+var interpreter = function () {
+	var db=new Set();
+	var parser=new Parser();
 
 	this.parseDB = function (params, paramss, paramsss) {
-		var buffer_input=parse_name_arguments(params);
-		var preposition=buffer_input[0];
-		preposition["rules"]=[];	
-		var input=buffer_input[1];
-		var regex=rule;
-		while (input.match(regex)){
-			input.replace(regex, "");
-			buffer_input=parse_name_argumentos(input);
-			var rule=buffer_input[0];
-			var order_of_variables=[];
-			input=baffer_input[1];
-			rule["args"].forEach(function(arg, indice, array){
-				order_of_variables.push(preposition["args"].indexOf(arg));
-			})
-			preposition["rules"].push([rule["name"], order_of_variables]);
-			regex=next_rule;
-		}
-		if(!input.match(fin)){
-			throw Error();
-		}
-		getPrepositionFromBuffer(preposition);
+		params.forEach(function(sentence) {
+			var rule=parser.parseRule(sentence);
+			if(rule){
+				if(!db[rule.name])
+					db[rule.name]=new Relationship(rule.name);
+				db[rule.name].setRules(rule.rules);
+				return;
+			}
+			var fact=parser.parseFact(sentence);
+			if(fact){
+				if(!db[fact.name])
+					db[fact.name]=new Relationship(fact.name);
+				db[fact.name].addFact(fact.fact);
+				return;
+			}
+			throw Error("malos datos");
+		});
 	};
+
 
 
 	this.checkQuery = function (params) {
-		return true;
+		var query=parser.parseFact(params);
+		throw Error("jj");
+		if(query){
+			return db[query.name].areRelated(query.fact);}
+		return false;
 	};
 
 }
 
-module.exports = Interpreter;
+module.exports = interpreter;
