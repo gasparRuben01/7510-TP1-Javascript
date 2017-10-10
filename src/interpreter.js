51acdda8b444bdafb18e9995d0ed6a0b21650132
gasparRuben01 <gaspar.zanellini@gmail.com>
2017-10-10 06:11:41 -0300

    mm

--- src/interpreter.js
+++ src/interpreter.js
@@ -15,7 +15,7 @@ class Relationship {
 		var fact=args.join(',');
 		if(this.facts.has(fact))
 			return true;
-		return this.rules ? this.rules.map(function(r){r.areRelated(args, db)}).indexOf(false) : false;
+		return this.rules ? this.rules.map(function(r) {return r.areRelated(args, db)}).indexOf(false)==-1 : false;
 	}
 
 }
@@ -26,15 +26,16 @@ class Rule {
 		this.orderOfargs=orderOfargs;
 	}	
 	areRelated(args, db){
-		if(args.length>=this.orderOfargs.length){
+		if(args.length<this.orderOfargs.length){
 			return false;
 		}
-		return db[relation] ? db[relation].areRelated(this.orderOfargs.map(function(pos){return args[pos]}), db) : false;
+		return db[this.relation] ? db[this.relation].areRelated(this.orderOfargs.map(function(pos) {return args[pos]}), db) : false;
 	}
 }
 
 var Parser=function(){
-	var RULESYMBOL=/:=/;
+	var FORBIDDENSYMBOL="@";
+	var RULESYMBOL=/:-/;
 	var NAME_ARGS=/\s*[A-Za-z]+\s*\(.*\)/;
 	var END=/\.$/;
 	var NAME=/^\s*([A-Za-z]+)\s*/;
@@ -58,20 +59,21 @@ var Parser=function(){
 	this.parseRule=function(sentence){
 			if(!sentence.match(END))
 				return null;
-				
 			sentence=sentence.replace(END, "");
 			sentence=sentence.split(RULESYMBOL);
 			if(sentence.length!=2)
 				return null;
-			sentence[1]=sentence[1].split(/,/);
-			sentence=sentence[0].concat(sentence[1]);
-			name_args=sentence.map(function(a){return getName(a) && getVars(a) ? {name:getName(a), args:getVars(a)} : null});
-			if(args.indexOf(null))
+			sentence[1]=sentence[1].replace(/(\(.*\)[^(]*),/, "$1"+FORBIDDENSYMBOL);
+			sentence[1]=sentence[1].split(new RegExp(FORBIDDENSYMBOL));
+			sentence[1].push(sentence[0]);
+			sentence=sentence[1];
+			name_args=sentence.map(function(a) {return getName(a) && getVars(a) ? {name:getName(a), args:getVars(a)} : null});
+			if(name_args.indexOf(null)!=-1)
 				return null;
-			var name=name_args[0]["name"];
-			var args=name_args[0]["args"];
-			name_args=name_args.slince(1);
-			var rules=name_args.map(function(x){ 
+			var name=name_args[name_args.length-1]["name"];
+			var args=name_args[name_args.length-1]["args"];
+			name_args=name_args.slice(0, name_args.length-1);
+			var rules=name_args.map(function(x) { 
 						var orderofargs=x.args.map(function(a){ return args.indexOf(a)!=-1 ? args.indexOf(a) : null});
 						return orderofargs.indexOf(null)!=-1 ?  null : new Rule(x.name, orderofargs);});
 			return rules.indexOf(null)!=-1 ? null : {name:name, rules:rules}};
@@ -81,6 +83,13 @@ var Parser=function(){
 			return null;
 		sentence=sentence.replace(END, "");
 		return getName(sentence) && getVars(sentence) ? {name:getName(sentence), fact:getVars(sentence)} : null;};
+
+	this.parseQuery=function(sentence){
+		var query=this.parseFact(sentence+".");
+		query.query=query.fact;
+		delete query.fact;
+		return query;
+	}
 }
 					
 
@@ -93,36 +102,37 @@ var Parser=function(){
 
 
 var interpreter = function () {
-	var db=new Set();
+	var db={};
 	var parser=new Parser();
 
 	this.parseDB = function (params, paramss, paramsss) {
-		params.forEach(function(sentence) {
+		for(var i=0; i<params.length; i++){
+			var sentence=params[i];
 			var rule=parser.parseRule(sentence);
 			if(rule){
 				if(!db[rule.name])
 					db[rule.name]=new Relationship(rule.name);
 				db[rule.name].setRules(rule.rules);
-				return;
+			}else{
+				var fact=parser.parseFact(sentence);
+				if(fact){
+					if(!db[fact.name])
+						db[fact.name]=new Relationship(fact.name);
+					db[fact.name].addFact(fact.fact);
+				}else{
+					throw Error("malos datos");
+				}
 			}
-			var fact=parser.parseFact(sentence);
-			if(fact){
-				if(!db[fact.name])
-					db[fact.name]=new Relationship(fact.name);
-				db[fact.name].addFact(fact.fact);
-				return;
-			}
-			throw Error("malos datos");
-		});
+		}
 	};
 
 
 
+
 	this.checkQuery = function (params) {
-		var query=parser.parseFact(params);
-		throw Error("jj");
-		if(query){
-			return db[query.name].areRelated(query.fact);}
+		var query=parser.parseQuery(params);
+		if(query && db[query.name]){
+			return db[query.name].areRelated(query.query, db);}
 		return false;
 	};
 
